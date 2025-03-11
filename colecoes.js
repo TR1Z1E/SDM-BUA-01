89e8d46f9804116ac4d9dcad51ac5844a014e5d2
TR1Z1E <beatriz.rodrigues.souza127@gmail.com>
2025-03-11 11:42:46 -0300

    Feat trabalhando com coleções

---
+++ colecoes.js
@@ -0,0 +1,35 @@
+let numeros = [10 , 20 , 30 , 40, 50]
+console.log(numeros[2])
+
+let frutas = ["maça", "Banana", "Morango"]
+
+
+//interando sobre os elementos do array
+frutas.forEach((fruta, index) =>{
+    console.log(`${index}: ${fruta}`);
+});
+
+
+//Adicionando elementos
+frutas.push("Uva")
+frutas.push("Melão")
+
+console.log(frutas)
+
+//Removendo elementos
+
+frutas.pop();
+frutas.shift();
+frutas.splice(2, 1)
+
+console.log(frutas)
+
+//
+
+let mapa = new Map()
+mapa.set("nome", "Joana")
+mapa.set( 42, "Idade")
+
+//Acessaando valores
+console.log(mapa.get("nome"))
+console.log(mapa.get(42))
