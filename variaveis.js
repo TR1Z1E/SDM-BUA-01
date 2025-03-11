a9c5e33ae039418b5763b18e789b7d1b132d1321
TR1Z1E <beatriz.rodrigues.souza127@gmail.com>
2025-03-11 10:12:36 -0300

    Feat: trabalhando com variáveis

---
+++ variaveis.js
@@ -0,0 +1,12 @@
+var idade = 20;
+let nome = "Beatriz";
+let ativo = true;
+const PI = 3.14;
+
+console.log(idade,nome, ativo,PI)
+
+if (true){
+    console.log(idade, nome)
+    PI = 3.2
+    console.log(PI)
+};
